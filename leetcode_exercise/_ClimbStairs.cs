21491e99b101be086d20554b4132712b2be53c2b
CaoDenk <39513475+CaoDenk@users.noreply.github.com>
2023-08-03 16:41:18 +0800

    subarray

--- leetcode_exercise/_ClimbStairs.cs
+++ leetcode_exercise/_ClimbStairs.cs
@@ -20,7 +20,6 @@ namespace leetcode_exercise
         int Recur(int n)
         {
             
-            //if(n == 0) return 0;
             if(n is 1 or 2)
             {
                 return 1;
