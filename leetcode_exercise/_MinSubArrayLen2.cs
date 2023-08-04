5b0bc7ca0734838ffa6cc4c6c500d9edcf6a7691
CaoDenk <39513475+CaoDenk@users.noreply.github.com>
2023-08-04 14:42:27 +0800

    subarray

---
+++ leetcode_exercise/_MinSubArrayLen2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_exercise
+{
+    internal class _MinSubArrayLen2
+    {
+        public int MinSubArrayLen(int target, int[] nums)
+        {
+            return 0;
+        }
+        public int MaxSubArray(int[] nums)
+        {
+            int pre = nums[0], ans = nums[0];
+            for (int i = 1; i < nums.Length; ++i)
+            {
+                if (pre > 0)
+                {
+                    pre += nums[i];
+                }
+                ans = Math.Max(ans, pre);
+            }
+            return ans;
+        }
+    }
+}
