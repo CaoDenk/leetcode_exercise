bc2a516a0a02b1d92260c1a68e103ee79f8f8c42
登科 曹 <CaoDenk@outlook.com>
2023-08-27 21:59:45 +0800

    direct

--- leetcode_exercise/CountAndSay_.cs
+++ leetcode_exercise/CountAndSay_.cs
@@ -14,7 +14,67 @@ namespace leetcode_exercise
     {
         public string CountAndSay(int n)
         {
-            return string.Empty;
+            return Qn(n);
+        }
+
+       
+
+        string Qn(int n)
+        {   if (n == 1) return "1";
+            string res =Qn(n-1);
+            int count = 0;
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            for (; i < res.Length; i++)
+            {
+                if (i > 0 && res[i] != res[i - 1])
+                {
+                    sb.Append($"{count}{res[i - 1]}");
+                    count = 1;
+
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            if (i > 0)
+                sb.Append($"{count}{res[^1]}");
+            return sb.ToString();
+        }
+
+        static string Test()
+        {
+            string res = "21";
+            int count = 0;
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            for (; i < res.Length; i++)
+            {
+                if (i > 0 && res[i] != res[i - 1])
+                {
+                    sb.Append($"{count}{res[i-1]}");
+                    count = 1;
+
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            if (i>0)
+                sb.Append($"{count}{res[^1]}");
+            return sb.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            CountAndSay_ c = new();
+            Console.WriteLine(c.CountAndSay(1));
+            Console.WriteLine(c.CountAndSay(2));
+            Console.WriteLine(c.CountAndSay(3));
+            Console.WriteLine(c.CountAndSay(4));
+            //Console.WriteLine(Test());
         }
     }
 }
---
+++ leetcode_exercise/SpiralOrder_.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_exercise
+{
+    enum Direction
+    {
+        RIGHT=0,
+        DOWN=1,
+        LEFT=2,
+        UP=3
+    }
+    internal class SpiralOrder_
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            bool[,] vis=new bool[matrix.Length,matrix[0].Length];
+            List<int> ans=new List<int>();
+            int direct = 0;
+            switch((Direction)direct)
+            {
+
+            }
+
+
+        }
+        
+
+    }
+}
