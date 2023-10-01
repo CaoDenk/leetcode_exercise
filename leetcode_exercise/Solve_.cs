836ff3062472f2869c4520f1d260209aed14bcf9
登科 曹 <CaoDenk@outlook.com>
2023-10-01 21:23:43 +0800

    130

---
+++ leetcode_exercise/Solve_.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_exercise
+{
+    /// <summary>
+    /// 130. 被围绕的区域
+    /// 挖坑
+    /// </summary>
+    internal class Solve_
+    {
+        public void Solve(char[][] board)
+        {
+
+        }
+        //bfs 方向 下 左右
+        bool bfs(char[][] board,int i,int j, bool[,] visited)
+        {
+            visited[i, j] = true;
+            //Queue<(int,int)>
+
+
+            return true;
+        }
+    }
+}
---
+++ leetcode_exercise/SumNumbers_.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_exercise
+{
+    /// <summary>
+    /// 129. 求根节点到叶节点数字之和
+    /// </summary>
+    internal class SumNumbers_
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            long sum=0;
+
+            PreVisit(root,new StringBuilder(),ref sum);
+
+            return (int)sum;
+        }
+        void PreVisit(TreeNode root, StringBuilder sb, ref long sum)
+        {
+            sb.Append(root.val);
+            if (root.left==null&&root.right==null)
+            {
+                Console.WriteLine(sb.ToString());
+                sum += long.Parse(sb.ToString());
+               
+            }
+           
+            if (root.left != null)
+            {
+                PreVisit(root.left, sb, ref sum);
+            }
+             
+            if(root.right != null)
+            {
+                PreVisit(root.right, sb, ref sum);
+            }
+            sb.Remove(sb.Length - 1, 1);
+
+
+        }
+        static void Main(string[] args)
+        {
+            SumNumbers_ s = new();
+            {
+                TreeNode root = Utils.Make(new int?[] { 1, 2, 3 });
+                Console.WriteLine(s.SumNumbers(root));
+            }
+            {
+                TreeNode root = Utils.Make(new int?[] { 4, 9, 0, 5, 1 });
+                Console.WriteLine(s.SumNumbers(root));
+            }
+        }
+    }
+}
